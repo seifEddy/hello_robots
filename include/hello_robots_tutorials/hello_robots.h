ca18a36192cad18c44a87c62628a156586c87de9
seifEddy <seiftinik@gmail.com>
2023-06-14 21:58:08 +0100

    Added ROS subscriber to the same class, which subscribes directly to the published messsage for demonstrations puposes

--- include/hello_robots_tutorials/hello_robots.h
+++ include/hello_robots_tutorials/hello_robots.h
@@ -7,18 +7,25 @@ class HelloRobots
         HelloRobots(ros::NodeHandle* nh);
         ~HelloRobots();
 
+        // This function will run every timestep
         void update(const ros::TimerEvent&);
+
+        // This function will be executed each time a new message is received
+        void message_callback(const std_msgs::String::ConstPtr& msg);
+
     private:
         ros::NodeHandle nh_;
         ros::Timer timer;
         std_msgs::String msg;
         ros::Publisher message_publisher;
+        ros::Subscriber message_subscriber;
 
 };
 
 HelloRobots::HelloRobots(ros::NodeHandle* nh) : nh_(*nh)
 {
     message_publisher = nh_.advertise<std_msgs::String>("/message", 10, true);
+    message_subscriber = nh_.subscribe<std_msgs::String>("/message", 10, &HelloRobots::message_callback, this);
     timer = nh_.createTimer(ros::Duration(1.0 / 30.0), &HelloRobots::update, this);
 }
 
@@ -32,4 +39,9 @@ void HelloRobots::update(const ros::TimerEvent& e)
     msg.data = "Hello Robot friend, How are you?";
 
     message_publisher.publish(msg);
+}
+
+void HelloRobots::message_callback(const std_msgs::String::ConstPtr& msg)
+{
+    ROS_INFO("MESSAGE RECEIVED [%s]", msg->data.c_str());
 }
